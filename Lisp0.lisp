7a30e21cffe28b46d23c280879e0543f1dc9d81c
Adam Scrivener <aascrivener@gmail.com>
2014-09-16 16:21:24 -0400

    finished problem 3

--- Lisp0.lisp
+++ Lisp0.lisp
@@ -24,3 +24,16 @@
 		((not (atom atm)) (format t "**First argument should be an atom, but was ~a~%" atm))
 		((not (listp lst)) 0)
 		(t (n-occurrences-helper atm lst))))
+
+(defun deriv1 (mono)
+	(let ((c (first mono)) (x (second mono)) (i (third mono)))
+	(cond
+		((not (listp mono)) (format t "**Argument must be a list.~%"))
+		((not (= (list-length mono) 3)) (format t "**Argument must have 3 elements.~%"))
+		((not (numberp c)) (format t "**First element in list must be a number.~%"))
+		((numberp x) (format t "**Second element in list cannot be a number.~%"))
+		((listp x) (format t "**Second element in list cannot itself be a list.~%"))
+		((not (integerp i)) (format t "**Third element in list must be a positive or negative integer.~%"))
+		((or (= c 0) (= i 0)) (cons 0 (cons x (cons 0 nil))))
+		(t (cons (* i c) (cons x (cons (- i 1) nil)))))
+	))
