477e38af7f64905bfe2021239065cc05bb2c3239
Adam Scrivener <aascrivener@gmail.com>
2014-09-16 15:32:59 -0400

    finished problem 2

--- Lisp0.lisp
+++ Lisp0.lisp
@@ -9,4 +9,18 @@
 (defun undefined (x)
 	(if (undefined-helper x)
 		t
-		(error "No instance of *undefined*!")))
+		(format t "**No instance of *undefined*!")))
+
+(defun n-occurrences-helper (atm lst)
+	(cond
+		((not lst) 0)
+		((listp lst)
+			(+ (n-occurrences-helper atm (car lst)) (n-occurrences-helper atm (cdr lst))))
+		((eql lst atm) 1)
+		(t 0)))
+
+(defun n-occurrences (atm lst)
+	(cond
+		((not (atom atm)) (format t "**First argument should be an atom, but was ~a~%" atm))
+		((not (listp lst)) 0)
+		(t (n-occurrences-helper atm lst))))
