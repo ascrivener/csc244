ed615cc9a68c00d1f2719bff476490455a5b0d25
Adam Scrivener <aascrivener@gmail.com>
2014-09-17 11:35:27 -0400

    Done with delete_zeroes, valid_poly?

--- Lisp0.lisp
+++ Lisp0.lisp
@@ -15,25 +15,33 @@
 	(cond
 		((not lst) 0)
 		((listp lst)
-			(+ (n-occurrences-helper atm (car lst)) (n-occurrences-helper atm (cdr lst))))
+			(+ (n-occurrences-helper atm (car lst)) 
+				(n-occurrences-helper atm (cdr lst))))
 		((eql lst atm) 1)
 		(t 0)))
 
 (defun n-occurrences (atm lst)
 	(cond
-		((not (atom atm)) (format t "**First argument should be an atom, but was ~a~%" atm))
+		((not (atom atm)) 
+			(format t "**First argument should be an atom, but was ~a~%" atm))
 		((not (listp lst)) 0)
 		(t (n-occurrences-helper atm lst))))
 
 (defun valid_mono? (mono)
 	(let ((c (first mono)) (x (second mono)) (i (third mono)))
 		(cond
-			((not (listp mono)) (format t "**Argument must be a list.~%"))
-			((not (= (list-length mono) 3)) (format t "**Argument must have 3 elements.~%"))
-			((not (numberp c)) (format t "**First element in list must be a number.~%"))
-			((numberp x) (format t "**Second element in list cannot be a number.~%"))
-			((listp x) (format t "**Second element in list cannot itself be a list.~%"))
-			((not (integerp i)) (format t "**Third element in list must be a positive or negative integer.~%"))
+			((not (listp mono)) 
+				(format t "**Argument must be a list.~%"))
+			((not (= (list-length mono) 3)) 
+				(format t "**Argument must have 3 elements.~%"))
+			((not (numberp c)) 
+				(format t "**First element in list must be a number.~%"))
+			((numberp x) 
+				(format t "**Second element in list cannot be a number.~%"))
+			((listp x) 
+				(format t "**Second element in list cannot itself be a list.~%"))
+			((not (integerp i)) 
+				(format t "**Third element in list must be a positive or negative integer.~%"))
 		(t t))
 	))
 
@@ -47,22 +55,25 @@
 
 (defun valid_poly_helper (poly)
 	(cond
-		((not poly) t)
+		((not poly) (format t "Polynomial cannot be empty!"))
 		((and (listp poly) (valid_mono? (car poly))) 
 			(valid_poly? poly (second (car poly))))
-		(t nil)))
+		(t (format t "Polynomial must be a list of monomials!"))))
 
 (defun valid_poly? (poly var)
 	(cond
 		((not poly) t)
-		((listp poly) (and (and (valid_mono? (car poly)) 
+		(t (and (and (valid_mono? (car poly)) 
 							(eql var (second (car poly))))
-						(valid_poly? (cdr poly) var)))
-		(t nil)
-		))
+						(valid_poly? (cdr poly) var)))))
 
-(defun sort_poly (poly))
+(defun delete_zeroes (poly)
+	(cond
+		((not poly) nil)
+		((and (eq (third (car poly)) 0) (eq (first (car poly)) 0))
+			(delete_zeroes (cdr poly)))
+		(t (cons (car poly) (delete_zeroes (cdr poly))))))
 
 (defun simplify (poly)
 	(if (valid_poly? poly)
-		(regularize (delete_zeroes (sort_poly poly)))))
+		(regularize (delete_zeroes (sort poly #'> :key #'third)))))
